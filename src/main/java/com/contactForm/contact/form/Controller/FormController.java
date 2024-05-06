3b283be91584f27e1a5e56d35b9d9418b0d78f87
tusharPanchalAppSphere <150894260+tusharPanchalAppSphere@users.noreply.github.com>
2024-05-06 15:12:34 +0530

    Update FormController.java
    corrected the annotation

--- src/main/java/com/contactForm/contact/form/Controller/FormController.java
+++ src/main/java/com/contactForm/contact/form/Controller/FormController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Objects;
 
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 @RestController
 public class FormController {
 
